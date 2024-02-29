a6894b519a67c6c2e743240d6ada59c7de2809b8
mustafemohamoud <mustyymo@outlook.com>
2024-02-29 20:47:08 +0000

    folders set up

---
+++ my-react-app/src/components/AboutMe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <img
+        src="url_to_your_avatar_image.jpg"
+        alt="Developer Avatar"
+        style={{ width: '200px', borderRadius: '50%' }}
+      />
+      <p>
+        Hi, I'm Mustafe Mohamoud, a passionate web developer. Lorem ipsum dolor sit
+        amet, consectetur adipiscing elit. Nulla facilisi. Mauris facilisis
+        risus a libero rutrum, vitae malesuada velit finibus. Sed bibendum
+        facilisis neque, et feugiat libero ultricies ut. Vivamus sed orci vel
+        justo sollicitudin fermentum vel eu velit.
+      </p>
+    </div>
+  );
+};
+
+export default About;
